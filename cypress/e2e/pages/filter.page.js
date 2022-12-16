4398343a3696865a7d103ffc8380297f7bae38f7
NicMait <nicolemtlnd@gmail.com>
2022-12-16 18:24:10 -0500

    first commit

---
+++ cypress/e2e/pages/filter.page.js
@@ -0,0 +1,9 @@
+class Sort{
+
+	get sortOptions () { return ('#sort')}
+	get productList () { return ('.css-1n64n71')}
+	get priceList () { return (' p[class="chakra-text css-0"]')}
+	
+}
+
+export default new Sort()
