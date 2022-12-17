da3cdd5543cfff54efd331dec21f4e646a2daf76
NicMait <nicolemtlnd@gmail.com>
2022-12-17 12:26:42 -0500

    commit

--- cypress/e2e/pages/wishlist.page.js
+++ cypress/e2e/pages/wishlist.page.js
@@ -1,23 +1,24 @@
 class Wishlist{
 
 	get faveBtn () { return ('#top-favorite')}
+	// get faveConfirm() { return ()}
 	get faveHeader () { return ('.chakra-heading.css-11cq7yk')}
 	get mousepadFave () { return ('div[id="product-2"] div[class="css-1m8iww1"]')}
 	get pillowFave () { return ('div[id="product-4"] div[class="css-1m8iww1"]')}
 	get faveCount () { return ('button[id="top-favorite"] p[class="chakra-text css-0"]')}
 	get vneckFave () { return ('.css-1m8iww1')}
+	get removeFave () { return ('#remove-favorite-btn')}
+	get emptyFave () { return ('div[class="chakra-stack css-owjkmg"] h2[class="chakra-heading css-kmq9po"]')}
+	get mousepad () { return ('div[class="css-12qzrsi"] div:nth-child(1) div:nth-child(1) p:nth-child(1)')}
 
 	fave(){
-    cy.get(this.fullName).type('Martin Odegaard')
-		cy.get(this.emailAddress).type('marsenal2@yopmail.com')
-		cy.get(this.streetAddress).type('Gokasho')
-		cy.get(this.aptNumber).type('41')
-		cy.get(this.city).type('Higashiomi')
-		cy.get(this.country).select('Japan')
-		cy.get(this.province).type('Shiga')
-		cy.get(this.zipCode).type('529-1000')
+    cy.get(this.mousepadFave).click()
+		cy.get(this.faveCount).should('be.visible')
+		cy.get(this.faveCount).should('have.text',"Favorites [1]")
+		cy.get(this.faveBtn).click()
+		cy.get(this.faveHeader).should('have.text', "Favorites")
+	}
 
 }
-
 
 export default new Wishlist()
--- cypress/e2e/tests/wishlist.cy.js
+++ cypress/e2e/tests/wishlist.cy.js
@@ -26,42 +26,62 @@ describe('Wishlist', () => {
 
 	it('Add a product to favorites from the product details page', () => {
 		// Login with valid credentials
-	cy.get(Authentication.signInRegisterBtn).should('be.visible')
-	cy.get(Authentication.signInRegisterBtn).click()
-	Authentication.login('marsenal2@yopmail.com', 'Password123!')
-	cy.url().should('contain', 'products')
+	  cy.get(Authentication.signInRegisterBtn).should('be.visible')
+	  cy.get(Authentication.signInRegisterBtn).click()
+	  Authentication.login('marsenal2@yopmail.com', 'Password123!')
+	  cy.url().should('contain', 'products')
 
-	//View product details
-  cy.get(Gallery.vneckDetails).click()
-	cy.url().should('contain', 'quality-tshirt-vneck')
-	cy.get(Gallery.vneckHeader).should('be.visible')
-	cy.url().should('contain', 'quality-tshirt-vneck')
+	  //View product details
+    cy.get(Gallery.vneckDetails).click()
+	  cy.url().should('contain', 'quality-tshirt-vneck')
 
-	//Add item to faves
-	cy.get(Wishlist.vneckFave).should('be.visible')
-	cy.get(Wishlist.vneckFave).click()//add vneck to favorites
+	  //Add item to faves
+	  cy.get(Wishlist.vneckFave).should('be.visible')
+	   cy.wait(3000)
+	  cy.get(Wishlist.vneckFave).click()//add vneck to favorites
 
-	//confirm addition to faves
+	 //confirm addition to faves
 
-	cy.get(Wishlist.faveCount).should('have.text',"Favorites [1]")
+	  cy.get(Wishlist.faveCount).should('have.text',"Favorites [1]")
 
-})
+  })
 
-it('Remove an item from favorites', () => {
-	// Login with valid credentials
-cy.get(Authentication.signInRegisterBtn).should('be.visible')
-cy.get(Authentication.signInRegisterBtn).click()
-Authentication.login('marsenal2@yopmail.com', 'Password123!')
-cy.url().should('contain', 'products')
+  it('Remove an item from favorites', () => {
+	   // Login with valid credentials
+   cy.get(Authentication.signInRegisterBtn).should('be.visible')
+   cy.get(Authentication.signInRegisterBtn).click()
+   Authentication.login('marsenal2@yopmail.com', 'Password123!')
+   cy.url().should('contain', 'products')
 
-//Add a product to favorites from the product gallery
+   //Add a product to favorites
+   Wishlist.fave()
 
-cy.get(Wishlist.mousepadFave).should('be.visible')
-cy.get(Wishlist.mousepadFave).click()
+   //Remove item
+   cy.get(Wishlist.removeFave).should('be.visible')
+   cy.get(Wishlist.removeFave).click()
+   cy.get(Wishlist.emptyFave).should('have.text',"No favorites added!")
 
-//confirm addition to favorites
-cy.get(Wishlist.faveCount).should('have.text',"Favorites [1]")
+  })
 
-})
+  it.only('Add multiple items to favorites', () => {
+	    // Login with valid credentials
+   cy.get(Authentication.signInRegisterBtn).should('be.visible')
+   cy.get(Authentication.signInRegisterBtn).click()
+   Authentication.login('marsenal2@yopmail.com', 'Password123!')
+   cy.url().should('contain', 'products')
+
+     //Add products to favorites
+   cy.get(Wishlist.mousepadFave).should('be.visible')
+   cy.get(Wishlist.mousepadFave).click()
+   cy.get(Wishlist.faveCount).should('have.text',"Favorites [1]")
+   cy.get(Wishlist.pillowFave).should('be.visible')
+   cy.get(Wishlist.pillowFave).click()
+   cy.get(Wishlist.faveCount).should('have.text',"Favorites [2]")
+
+	   //Confirm the additions
+	  cy.get(Wishlist.faveBtn).click()
+
+
+	})
 
 })
